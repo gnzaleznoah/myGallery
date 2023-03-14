ec806698a9f7be40f5027dc49a20cba2c60de60d
Noah Gonzalez <gnzalezmnoah@gmail.com>
2023-03-14 13:26:34 -0700

    container function

---
+++ main.js
@@ -0,0 +1,13 @@
+let galleryImages = document.querySelectorAll('.gallery-cell');
+let getLatesOpenedImg;
+let windowWidth = window.innerWidth;
+
+galleryImages.forEach(function(image, index){
+    image.onclick = function({
+        getLatestOpenedImg = index +1;
+
+        let container = document.body;
+        let newImgWindow = document.createElement('div');
+        container.appendChild(newImgWindow);
+    })
+})
