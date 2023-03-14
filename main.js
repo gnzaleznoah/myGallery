211fcb1dc359e4628c4747b2f1ae69f42f488c23
Noah Gonzalez <gnzalezmnoah@gmail.com>
2023-03-14 13:27:51 -0700

    missing paranthases

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ let getLatesOpenedImg;
 let windowWidth = window.innerWidth;
 
 galleryImages.forEach(function(image, index){
-    image.onclick = function({
+    image.onclick = function(){
         getLatestOpenedImg = index +1;
 
         let container = document.body;
         let newImgWindow = document.createElement('div');
         container.appendChild(newImgWindow);
-    })
+    }
 })
